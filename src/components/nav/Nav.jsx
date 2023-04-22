7a3758896f3ab3da6ae39c8bb2cd73d3b85efa3b
mman9our <hack_225@outlook.sa>
2023-04-22 17:19:36 +0300

    Update the responsive and meida queries for skills

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,7 +7,7 @@ import { RiServiceFill } from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 import { useState } from 'react'
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={ activeNav === '#' ? 'active' : '' } ><AiOutlineHome /></a>
